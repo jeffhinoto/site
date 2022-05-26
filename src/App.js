7dd8ff932387d12727186a613a1638cd941dcb25
jeffhinoto <63283396+jeffhinoto@users.noreply.github.com>
2022-05-26 14:58:24 -0300

    fix do fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   
   return (
     <>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css"></link>
     <section id='main'>
     <div className='App'>     
 
@@ -15,19 +16,73 @@ function App() {
             <i><h4>Trazendo vida ao seu projeto</h4></i>
         </div>
         <div style={{paddingTop:'100px',}}></div>
+        <div id='contact-info'>
+    <div className='links'>
+      <a href='https://www.linkedin.com/in/jeffhinoto' target='_blank' className='orange-border'>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span>
+                <i className="fab fa-linkedin-in"></i>
+              </span>
+        </div>
+      </a>
+      <a href='https://github.com/jeffhinoto' target='_blank' className='orange-border' >
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span>
+                <i className="fab fa-github"></i>
+              </span>
 
+        </div>
+      </a>
+      <a href='https://discord.gg/rAQFxeACpY' className='orange-border' target='_blank'>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span>
+                <i className="fab fa-discord"></i>
+              </span>
+        </div>
+      </a>
+      <a href='https://www.instagram.com/jeffhinoto' className='orange-border' target='_blank'>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span>
+                <i className="fab fa-instagram"></i>
+              </span>
+        </div>
+      </a>
+      <a href='https://www.youtube.com/jeffhinotoo' className='orange-border' target='_blank'>
+        <div>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span>
+                <i className="fab fa-youtube"></i>
+              </span>
+        </div>
+      </a>
+    </div>
+  </div>
+    
     </div>
 
     </section>
-    
-    <section id='contact'>
-    <a class="arrow-wrap img" href="#contact">
-            <span class="arrow"></span></a>
-    <div className='contact'> 
-    <div><iframe src="https://discord.com/widget?id=979197828234616852&theme=dark" width="300" height="300" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe></div>
-    
+    <div style={{color:"#fff",display:'inline-table'}}>
+      <span>teste</span>
     </div>
-    </section>
     </>
   );
 }
