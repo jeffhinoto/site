fbf105520056a729d40bef92e5bc55650a509942
jeffhinoto <63283396+jeffhinoto@users.noreply.github.com>
2022-05-26 01:04:44 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import logo from './img.png';
 import './App.css';
 
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <section id='main'>
+    <div className='App'>     
+
+      <img src={logo} className='img'/>
+     <h1><i><span className='name'>JEFF HINOTO</span></i></h1>
+      <div className="typewriter">
+            <i><h4>Trazendo vida ao seu projeto</h4></i>
+        </div>
+        <div style={{paddingTop:'100px',}}></div>
+
     </div>
+
+    </section>
+    
+    <section id='contact'>
+    <a class="arrow-wrap img" href="#contact">
+            <span class="arrow"></span></a>
+    <div className='contact'> 
+    <div><iframe src="https://discord.com/widget?id=979197828234616852&theme=dark" width="300" height="300" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe></div>
+    
+    </div>
+    </section>
+    </>
   );
 }
 
